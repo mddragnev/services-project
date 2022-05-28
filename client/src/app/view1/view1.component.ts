fbf433a8b88b3ae04ecc66c6c51912acd3d2e4ae
Martin Dragnev <mddragnev@gmail.com>
2022-05-28 14:25:16 +0300

    Add kinda final client side

--- client/src/app/view1/view1.component.ts
+++ client/src/app/view1/view1.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { debug } from 'console';
+import { IgxCardComponent } from 'igniteui-angular';
 import { TravelAppService } from '../services/travel-app.service';
 
 @Component({
@@ -7,7 +9,12 @@ import { TravelAppService } from '../services/travel-app.service';
   styleUrls: ['./view1.component.scss']
 })
 export class View1Component implements OnInit {
+  @ViewChild('languageSpan') public lang: any;
+  @ViewChildren(IgxCardComponent, {read: ElementRef})
+  public cards!: QueryList<ElementRef>;
   public travelAppDestinations: any = null;
+
+  private language = '';
 
   constructor(
     private travelAppService: TravelAppService,
@@ -16,5 +23,26 @@ export class View1Component implements OnInit {
   ngOnInit() {
     // depending on implementation, data subscriptions might need to be unsubbed later
     this.travelAppService.getData('Destinations').subscribe(data => this.travelAppDestinations = data);
+  }
+
+  public changeLanguage(event: any) {
+    // debugger;
+    event.preventDefault();
+    const classList = this.lang.nativeElement.classList.value;
+    if (classList.includes('bg')) {
+      this.language = 'us';
+      this.cards.forEach(card => {
+        card.nativeElement.querySelector('span').classList.remove('fi-bg');
+        card.nativeElement.querySelector('span').classList.add('fi-us');
+
+      });
+    } else {
+      this.language = 'bg';
+      this.cards.forEach(card => {
+        card.nativeElement.querySelector('span').classList.remove('fi-us');
+        card.nativeElement.querySelector('span').classList.add('fi-bg');
+
+      });
+    }
   }
 }
